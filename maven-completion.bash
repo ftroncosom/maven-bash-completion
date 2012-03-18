7d3873776405d52c532eaf0576cc24b35d040174
Francisco Collao Garate <pcollaog@gmail.com>
2012-03-17 22:36:32 -0300

    Added more functions

--- maven-completion.bash
+++ maven-completion.bash
@@ -8,20 +8,43 @@ _mvn(){
 	COMPREPLY=()
 	local cur="${COMP_WORDS[COMP_CWORD]}"
 	local prev="${COMP_WORDS[COMP_CWORD-1]}"
-	local opts="clean install package test jetty:run site:site"
+	local opts="clean install package test jetty:run site:site javadoc"
 
 	case "${prev}" in
 		clean)
-			COMPREPLY=( $(compgen -f ${cur}) )
+			local clean_opts="install package deploy test compile test-compile"
+			COMPREPLY=($(compgen -W "${clean_opts}"))
 			return 0
 			;;
 		install)
+			local install_opts="package deploy test -DskipTest=true"
+			COMPREPLY=($(compgen -W "${install_opts}"))
+			return 0
+			;;
+		deploy)
+			local deploy_opts="-DskipTests=true"
+			COMPREPLY=(${deploy_opts})
+			return 0
+			;;
+
+		javadoc)
+			local javadoc_opts="javadoc jar test-javadoc"
+			COMPREPLY=($(compgen -W "${javadoc_opts}"))
+			return 0
+			;;
+		*)
+			COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+			return 0
 			;;
 	esac
 
-	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
 
-}
+	if [[ "${cur}" == -*  ]] ;then
+		COMPREPLY=( $( compgen -W '-DskipTest=true' -- "${cur}") )
+	fi
+
+	return 0
+} &&
 
 complete -F _mvn mvn
 complete -F _mvn mvnDebug
